7f06c4909d3a21d5ca0c6aa2525ad48c89916cdc
Hamanpreet <haman42626@gmail.com>
2023-09-20 20:09:32 -0400

    Set up the two_player_ruby_project

---
+++ player_game.rb
@@ -0,0 +1,18 @@
+class Player
+#createPlayer
+#store data for players
+
+end
+
+class MathQuestion
+#createQues,
+#store questions and check if user gave right answer
+#user i/o
+end
+
+class Game
+# create new Game
+#stores score of both players, determine when game should end
+# manage current_player
+
+end
